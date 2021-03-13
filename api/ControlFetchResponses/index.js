8a810446f06005372e6b3490cec19de1441700d7
dylankerr-bis <dylankerr-bis@users.noreply.github.com>
2021-03-13 15:04:21 +0100

    Include total response count in FetchResponses

--- api/ControlFetchResponses/index.js
+++ api/ControlFetchResponses/index.js
@@ -1,19 +1,21 @@
 module.exports = async function (context, req, responses) {
-    const aggregatedResponses = responses
-        .filter(response => Number.isInteger((response || {}).OptionId))
-        .reduce(
-            (acc, response) => {
-                acc[response.OptionId] = (acc[response.OptionId] || 0) + 1;
-                return acc;
-            },
-            {}
-        );
+    const validResponses = responses.filter(
+        response => Number.isInteger((response || {}).OptionId)
+    );
+    const aggregatedResponses = validResponses.reduce(
+        (acc, response) => {
+            acc[response.OptionId] = (acc[response.OptionId] || 0) + 1;
+            return acc;
+        },
+        {}
+    );
 
     context.res = {
         headers: {
             'Content-Type': 'application/json'
         },
         body: {
+            responseCount: validResponses.length,
             aggregatedResponses,
         },
     };
