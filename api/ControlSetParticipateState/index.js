334f7dabaa171fbaa7f5c2d223eb18beb63767ee
dylankerr-bis <dylankerr-bis@users.noreply.github.com>
2021-03-14 01:03:02 +0100

    Implement Broadcast view

--- api/ControlSetParticipateState/index.js
+++ api/ControlSetParticipateState/index.js
@@ -1,14 +1,5 @@
 module.exports = async function (context, req) {
     const state = (req.body || {}).state;
-
-    if (!state) {
-        context.res = {
-            status: 400,
-            body: "Missing `state` field"
-        };
-        return;
-    }
-
     return {
         "target": "setState",
         "arguments": [ state ],
